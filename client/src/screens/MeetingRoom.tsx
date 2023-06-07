291f1dd210e75bdd157ed2b058a17fb3864bcbbd
Nguyen Khanh <nguyenkhanh17112003@gmail.com>
2023-06-08 00:49:51 +0700

    add camera component and setup socket id room and user id

--- client/src/screens/MeetingRoom.tsx
+++ client/src/screens/MeetingRoom.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { Alert, SafeAreaView, StyleSheet, Text } from "react-native";
 import { io } from "socket.io-client";
 import Header from "../apps/MeetingRoom/Header";
 import OptionMeeting from "../apps/MeetingRoom/OptionMeeting";
+import { Camera } from "expo-camera";
 
 type Props = {
     navigation: any;
@@ -13,32 +14,59 @@ let socket: any;
 const MeetingRoom = function ({ navigation }: Props) {
     const [name, setName] = useState<string>("");
     const [roomId, setRoomId] = useState<string>("");
+    const [activeUser, setActiveUser] = useState();
+    const [startCamera, setStartCamera] = useState<boolean>(false);
+
+    const __startCamera = async function () {
+        const { status } = await Camera.requestCameraPermissionsAsync();
+        if (status === "granted") {
+            setStartCamera(true);
+        } else {
+            Alert.alert("Access denied");
+        }
+    };
 
     const joinRoom = function () {
+        __startCamera();
         socket.emit("join-room", {
             roomId: roomId,
-            name: name,
+            userName: name,
         });
     };
 
     useEffect(() => {
-        socket = io("http://localhost:3000", {
+        socket = io("https://b5a7-14-163-193-228.ap.ngrok.io", {
             reconnection: true,
         });
         socket.on("connection", () => {
             return console.log("connected");
         });
+
+        // socket.on("all-users", function (users: any) {
+        //     console.log("avtive user");
+        //     console.log(users);
+        //     setActiveUser(users);
+        // });
     }, []);
     return (
         <SafeAreaView style={styles.container}>
             <Header navigation={navigation} />
-            <OptionMeeting
-                name={name}
-                roomId={roomId}
-                setName={setName}
-                setRoomId={setRoomId}
-                joinRoom={joinRoom}
-            />
+            {startCamera ? (
+                <SafeAreaView>
+                    <Camera
+                        type="front"
+                        style={{ width: "100%", height: 600 }}
+                    ></Camera>
+                </SafeAreaView>
+            ) : (
+                <OptionMeeting
+                    name={name}
+                    roomId={roomId}
+                    setName={setName}
+                    setRoomId={setRoomId}
+                    joinRoom={joinRoom}
+                />
+            )}
         </SafeAreaView>
     );
 };
