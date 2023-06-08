9d9f457fa8fd0c02497d63aedc548350e3045c00
Nguyen Khanh <nguyenkhanh17112003@gmail.com>
2023-06-08 10:58:33 +0700

    config babel and update chat components

---
+++ client/src/apps/MeetingRoom/Chat.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+
+type Props = {};
+
+const Chat = function ({}: Props) {
+    return (
+        <View>
+            <SafeAreaView></SafeAreaView>
+        </View>
+    );
+};
+
+export default Chat;
+
+const styles = StyleSheet.create({
+    container: {},
+});
--- client/src/screens/MeetingRoom.tsx
+++ client/src/screens/MeetingRoom.tsx
@@ -1,21 +1,59 @@
 import React, { useState, useEffect } from "react";
-import { Alert, SafeAreaView, StyleSheet, Text } from "react-native";
+import {
+    Alert,
+    SafeAreaView,
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Modal,
+} from "react-native";
 import { io } from "socket.io-client";
 import Header from "../apps/MeetingRoom/Header";
+import Chat from "../apps/MeetingRoom/Chat";
 import OptionMeeting from "../apps/MeetingRoom/OptionMeeting";
 import { Camera } from "expo-camera";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 type Props = {
     navigation: any;
 };
+
+const menuIcons = [
+    {
+        id: 1,
+        name: "microphone",
+        title: "Mute",
+        customColor: "#fefefe",
+    },
+    {
+        id: 2,
+        name: "video-camera",
+        title: "Stop Video",
+        customColor: "#fefefe",
+    },
+    {
+        id: 3,
+        name: "upload",
+        title: "Share Content",
+        customColor: "#fefefe",
+    },
+    {
+        id: 4,
+        name: "group",
+        title: "Participants",
+        customColor: "#fefefe",
+    },
+];
 
 let socket: any;
 
 const MeetingRoom = function ({ navigation }: Props) {
     const [name, setName] = useState<string>("");
     const [roomId, setRoomId] = useState<string>("");
-    const [activeUser, setActiveUser] = useState();
+    const [activeUsers, setActiveUsers] = useState<any>([]);
     const [startCamera, setStartCamera] = useState<boolean>(false);
+    const [modalVisibale, setModalVisible] = useState<boolean>(false);
 
     const __startCamera = async function () {
         const { status } = await Camera.requestCameraPermissionsAsync();
@@ -35,28 +73,93 @@ const MeetingRoom = function ({ navigation }: Props) {
     };
 
     useEffect(() => {
-        socket = io("https://b5a7-14-163-193-228.ap.ngrok.io", {
+        socket = io("https://f90f-14-163-193-228.ap.ngrok.io", {
             reconnection: true,
         });
         socket.on("connection", () => {
             return console.log("connected");
         });
 
-        // socket.on("all-users", function (users: any) {
-        //     console.log("avtive user");
-        //     console.log(users);
-        //     setActiveUser(users);
-        // });
+        socket.on("all-users", function (users: any) {
+            setActiveUsers(users);
+        });
     }, []);
     return (
         <SafeAreaView style={styles.container}>
             <Header navigation={navigation} />
             {startCamera ? (
-                <SafeAreaView>
-                    <Camera
-                        type="front"
-                        style={{ width: "100%", height: 600 }}
-                    ></Camera>
+                <SafeAreaView style={styles.wrapper}>
+                    <Modal
+                        animationType="slide"
+                        transparent={false}
+                        presentationStyle="fullScreen"
+                        visible={modalVisibale}
+                        onRequestClose={function () {
+                            setModalVisible(!modalVisibale);
+                        }}
+                    >
+                        <Chat />
+                    </Modal>
+
+                    <View style={styles.activeUsersContainer}>
+                        <View style={styles.cameraContainer}>
+                            <Camera
+                                type="front"
+                                style={{
+                                    width:
+                                        activeUsers.length <= 1 ? "100%" : 200,
+                                    height: activeUsers.length <= 1 ? 600 : 200,
+                                }}
+                            ></Camera>
+                            {activeUsers
+                                ?.filter((user: any) => user.userName != name)
+                                ?.map(function (user: any, index: number) {
+                                    return (
+                                        <View
+                                            key={index}
+                                            style={styles.activeUserContainer}
+                                        >
+                                            <Text style={{ color: "white" }}>
+                                                {user.userName}
+                                            </Text>
+                                        </View>
+                                    );
+                                })}
+                        </View>
+                    </View>
+
+                    <View style={styles.menu}>
+                        {menuIcons.map(function (icon, index) {
+                            return (
+                                <TouchableOpacity
+                                    key={index}
+                                    style={styles.title}
+                                >
+                                    <FontAwesome
+                                        name={icon.name}
+                                        size={24}
+                                        color={"#efefef"}
+                                    />
+                                    <Text style={styles.textTitle}>
+                                        {icon.title}
+                                    </Text>
+                                </TouchableOpacity>
+                            );
+                        })}
+                        <TouchableOpacity
+                            onPress={function () {
+                                return setModalVisible(true);
+                            }}
+                            style={styles.title}
+                        >
+                            <FontAwesome
+                                name="comment"
+                                size={24}
+                                color={"#efefef"}
+                            />
+                            <Text style={styles.textTitle}>Chat</Text>
+                        </TouchableOpacity>
+                    </View>
                 </SafeAreaView>
             ) : (
                 <OptionMeeting
@@ -77,5 +180,51 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: "#223243",
         flex: 1,
+    },
+    wrapper: {
+        flex: 1,
+    },
+
+    camera: {
+        height: "100%",
+    },
+    menu: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+    },
+    title: {
+        justifyContent: "center",
+        alignItems: "center",
+        height: 50,
+        marginTop: 15,
+    },
+    textTitle: {
+        color: "white",
+        marginTop: 10,
+    },
+
+    activeUsersContainer: {
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "black",
+        width: "100%",
+        alignItems: "center",
+    },
+
+    cameraContainer: {
+        backgroundColor: "black",
+        justifyContent: "center",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignItems: "center",
+    },
+    activeUserContainer: {
+        borderColor: "gray",
+        borderWidth: 1,
+        width: 200,
+        height: 200,
+        backgroundColor: "#223243",
+        justifyContent: "center",
+        alignItems: "center",
     },
 });
