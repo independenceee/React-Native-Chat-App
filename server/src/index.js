75da0d2cac09c1ff5894ab026efc511ff010cf4c
Nguyen Khanh <nguyenkhanh17112003@gmail.com>
2023-06-08 12:19:09 +0700

    add user and update message

--- server/src/index.js
+++ server/src/index.js
@@ -13,18 +13,45 @@ app.get("/", (request, response) => {
     response.send("Server");
 })
 
+const users = [];
+const addUser = function(userName, roomId) {
+    users.push(
+        {
+            userName: userName, 
+            roomId: roomId
+        }
+    )
+}
+
+const userleave = function(userName) {
+    users = users.filter(user => user.userName != userName)
+}
+
+const getRoomUsers = function(roomId) {
+    return users.filter((user)=> (user.roomId == roomId))
+}
+
 
 
 io.on("connection", (socket) => {
     console.log("Connected");
     socket.on("join-room", ({roomId, userName}) => {
-        console.log("user joined room");
-        console.log(roomId, userName);
-        socket.join(roomId);
-        socket.to(roomId).emit("user-connected", userName)
+        if(roomId && userName) {
+            socket.join(roomId);
+            addUser(userName, roomId)
+            socket.to(roomId).emit("user-connected", userName);
+            io.to(roomId).emit("all-users", getRoomUsers(roomId));
+        }
+        socket.on("disconnect", function() {
+            console.log("Connected");
+            socket.leave(roomId);
+            userleave(userName);
+            io.to(roomId).emit("all-users", getRoomUsers(roomId))
+        })
     })
 })
 
 server.listen(PORT, function() {
     console.log(`http://localhost:${PORT}`)
 })
+
